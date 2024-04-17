8df6a0d8d5677a023ddcf96816a0067676e40876
Sidney Liu <cloxnu@outlook.com>
2024-04-18 00:10:26 +0800

    Update

--- prestart.sh
+++ prestart.sh
@@ -1,2 +1,2 @@
-npx esbuild --bundle ./src/index.ts --outdir=dist --minify --sourcemap --platform=node
 npx prisma generate
+npx esbuild --bundle ./src/index.ts --outdir=dist --minify --sourcemap --platform=node
